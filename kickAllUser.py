af33f203efff974746afe0f6b89b3d9917daad9d
chinghsuanwei <chinghsuanwei@gmail.com>
2022-04-24 13:50:56 +0800

    print the list of members on the server

--- kickAllUser.py
+++ kickAllUser.py
@@ -1,16 +1,37 @@
 import discord
 from discord.ext.commands import Bot
 
+intents = discord.Intents.default()
+intents.members = True
+
 #command start with prefix !
-bot = Bot("!")
+bot = Bot(command_prefix="!", intents=intents)
 
 @bot.command()
 async def hello(ctx):
-    await ctx.send("Command executed")
+    await ctx.send("Hello")
     
+@bot.command()    
+async def kick_all(ctx):
+    await ctx.send('Wait A Second...') # I am alive
+
+    # This is a check for user, if user is you or server owner will be 
+    await ctx.send('Started') # Start
+
+    members = ctx.guild.members      # This is list of members
+    print(members)
+    #members.remove(ctx.me)           # a user can't kick you'r self so remove bot
+    #print(members)
+
+    for member in members:           # creating a loop
+        print(member.id)
+        # Kick
+
+
 
 #get your token in the bot page
 bot.run('your_token')
+
 
 
 
